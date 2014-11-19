a23dc3c90508b01b20918478019b7a66c8e5caaa
Mauro Werder <mauro_lc@runbox.com>
2014-11-19 22:54:20 +0100

    added workaround example for issue #2

---
+++ examples/ex_issue2.jl
@@ -0,0 +1,68 @@
+######
+# workaround for issue #2
+##########
+using Traits
+
+@traitdef Foo_NotWorking{X} begin
+    fnxx{Y<:Integer}(X, Vector{Y}) -> Integer
+end
+
+# This errors because of https://github.com/JuliaLang/julia/issues/9043
+# 
+# @traitimpl Foo_NotWorking{Float64} begin
+#     fnxx(x::Float64, y::Array{Uint8,1}) = y[iround(x)]
+# end
+
+@traitdef Foo{X} begin
+    Y = getFooY(X) 
+    getFooY(Type{X}) -> DataType
+    fnx(X, Y) -> Integer
+    @constraints begin
+        eltype(Y)<:Integer
+    end
+end
+
+@traitimpl Foo{Float64} begin
+    getFooY(::Type{Float64}) = Array{Uint8,1}
+    fnx(x::Float64, y::Array{Uint8,1}) = y[iround(x)]
+end
+
+@traitimpl Foo{Int} begin
+    getFooY(::Type{Int}) = Array{Integer,1}
+    fnx(x::Int, y::Array{Integer,1}) = y[iround(x)]
+end
+
+@traitfn tf89{X, Y<:Integer; Foo{X}}(x::X, a::Vector{Y}) = fnx(x, a) + 5
+
+tf89(2., Uint8[1,2])
+tf89(2, Integer[1,2])
+
+# tf89(2., Int[1,2]) # errors
+
+## or this also works:
+
+@traitdef Bar{X} begin
+    Y = getBarY(X) 
+    getBarY(Type{X}) -> DataType
+    gnx(X, Vector{Y}) -> Integer
+    @constraints begin
+        eltype(Y)<:Integer
+    end
+end
+
+@traitimpl Bar{Float64} begin
+    getBarY(::Type{Float64}) = Uint8
+    gnx(x::Float64, y::Array{Uint8,1}) = y[iround(x)]
+end
+
+@traitimpl Bar{Int} begin
+    getBarY(::Type{Int}) = Integer
+    gnx(x::Int, y::Array{Integer,1}) = y[iround(x)]
+end
+
+@traitfn tf90{X, Y<:Integer; Bar{X}}(x::X, a::Vector{Y}) = gnx(x, a) + 5
+
+tf90(2., Uint8[1,2])
+tf90(2, Integer[1,2])
+
+# tf90(2., Int[1,2]) # errors
