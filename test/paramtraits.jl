d9210506e420909b892268dbca56328b2fea6212
Tony Fong <tony.hf.fong@gmail.com>
2015-01-13 21:27:13 +0700

    bugfix multi-parameter-parametric trait

--- test/paramtraits.jl
+++ test/paramtraits.jl
@@ -29,3 +29,24 @@ println( "test istrait{ SemiFunctor{Array{Int,1}}} == true" )
 @test istrait( SemiFunctor{Array{Int,1}} )
 println( "test istrait{ SemiFunctor{Array{Int,2}}} == false" )
 @test istrait( SemiFunctor{Array{Int,2}} ) == false
+
+println( "test multi-field traits each field is parametric")
+@traitdef MyParamTr1{X{Y}, Z{Y}} begin
+    @constraints begin
+        Y <: Number
+    end
+    ftest1( X{Y}, Z{Y} ) -> X{Z{Y}}
+end
+
+println( " ... test its implementation" )
+@traitimpl MyParamTr1{ Array{T...}, Nullable{T} } begin
+    ftest1{T}( a::Array{T,1}, x::Nullable{T} ) = isnull(x) ?
+            Nullable{T}[] : Nullable{T}[ Nullable(_*x.value) for _ in a ]
+end
+
+println( " ... check Array x Nullable" )
+@assert istrait( MyParamTr1{ Array{Int,1}, Nullable{Int} }, verbose=true )
+@assert ! istrait( MyParamTr1{ Array{Int,2}, Nullable{Int} } )
+@assert ! istrait( MyParamTr1{ Array{Int,1}, Nullable{Float64} } )
+println( " ... check dispatch")
+@assert isequal( ftest1( [1,2,3], Nullable(2) ),[Nullable(2),Nullable(4),Nullable(6)] )
